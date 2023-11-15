81bfa5faa4139ac48c75e51c54df56906367c95b
Kalantri <624604@cognizant.com>
2023-11-15 20:28:00 +0530

    optimizing and removing onvoking tests

--- images/linux/scripts/installers/python.sh
+++ images/linux/scripts/installers/python.sh
@@ -6,8 +6,8 @@
 
 set -e
 # Source the helpers for use with the script
-source $HELPER_SCRIPTS/etc-environment.sh
-source $HELPER_SCRIPTS/os.sh
+source "$HELPER_SCRIPTS"/etc-environment.sh
+source "$HELPER_SCRIPTS"/os.sh
 
 # Install Python, Python 3, pip, pip3
 apt-get install -y --no-install-recommends python3 python3-dev python3-pip python3-venv
@@ -31,4 +31,4 @@ fi
 # Adding this dir to PATH will make installed pip commands are immediately available.
 prependEtcEnvironmentPath '$HOME/.local/bin'
 
-invoke_tests "Tools" "Python"
+#invoke_tests "Tools" "Python"
