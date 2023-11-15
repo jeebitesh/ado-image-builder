93cbb9e6d717b829a0bb9068f8ef3741b82ad93d
Kalantri <624604@cognizant.com>
2023-11-15 14:43:01 +0530

    optimizing and adding tests

--- images/linux/scripts/installers/powershellcore.sh
+++ images/linux/scripts/installers/powershellcore.sh
@@ -4,9 +4,30 @@
 ##  Desc:  Installs powershellcore
 ################################################################################
 
-source $HELPER_SCRIPTS/install.sh
+source "$HELPER_SCRIPTS"/install.sh
+###################################
+# Prerequisites
 
-pwshversion=$(get_toolset_value .pwsh.version)
+# Update the list of packages
+apt-get update
+
+# Install pre-requisite packages.
+apt-get install -y install wget ca-certificates curl apt-transport-https software-properties-common lsb-release gnupg
+
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+
+# Register the Microsoft repository keys
+dpkg -i packages-microsoft-prod.deb
+
+# Delete the the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+apt-get update
 
 # Install Powershell
-apt-get install -y powershell=$pwshversion*
+apt-get install -y powershell azure-cli
