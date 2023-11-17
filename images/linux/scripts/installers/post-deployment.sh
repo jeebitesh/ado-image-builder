0f4f126f0cb54472e23e118542fdb31e6fa53a3b
Kalantri <624604@cognizant.com>
2023-11-17 09:31:29 +0530

    remove sudo

--- images/linux/scripts/installers/post-deployment.sh
+++ images/linux/scripts/installers/post-deployment.sh
@@ -11,13 +11,13 @@ chmod -R 777 /opt
 echo "chmod -R 777 /usr/share"
 chmod -R 777 /usr/share
 
-chmod 755 $IMAGE_FOLDER
+chmod 755 "$IMAGE_FOLDER"
 
 # Remove quotes around PATH
 ENVPATH=$(grep 'PATH=' /etc/environment | head -n 1 | sed -z 's/^PATH=*//')
 ENVPATH=${ENVPATH#"\""}
 ENVPATH=${ENVPATH%"\""}
-echo "PATH=$ENVPATH" | sudo tee -a /etc/environment
+echo "PATH=$ENVPATH" | tee -a /etc/environment
 echo "Updated /etc/environment: $(cat /etc/environment)"
 
 # Clean yarn and npm cache
