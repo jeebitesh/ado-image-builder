a96ad8d2c3cb72de42d49510b1c461bc0aeaeaa5
Kalantri <624604@cognizant.com>
2023-11-24 11:58:55 +0530

    Minor Upgrades to Linux and Windows image scripts

--- images/linux/scripts/installers/azure-devops-cli.sh
+++ images/linux/scripts/installers/azure-devops-cli.sh
@@ -10,9 +10,17 @@ export AZURE_EXTENSION_DIR=/opt/az/azcliextensions
 echo "AZURE_EXTENSION_DIR=$AZURE_EXTENSION_DIR" | tee -a /etc/environment
 
 # install azure devops Cli extension
-extensions=$(az extension list-available --query "[?preview == \`false\` && experimental == \`false\` && name != \`azure-cli-ml\`].{name:name}" -o json )
+extensions=$(az extension list-available --query "[?preview == \`false\` && experimental == \`false\` && name != \`azure-cli-ml\`].{name:name}" -o json)
 items=$(echo "$extensions" | jq -c -r '.[]')
-for item in ${items[@]}; do name=$(jq '.name' <<< "$item" | tr -d '"'); az extension add --upgrade --name "$name"; done
+for item in ${items[@]}; do
+    name=$(jq '.name' <<<"$item" | tr -d '"')
+    az extension add --upgrade --name "$name"
+done
 az aks install-cli
+# get kubelogin
+wget https://github.com/Azure/kubelogin/releases/download/v0.0.33/kubelogin-linux-amd64.zip
+unzip kubelogin-linux-amd64.zip
+sudo mv bin/linux_amd64/kubelogin /usr/bin
+kubelogin convert-kubeconfig -l azurecli
 
 #invoke_tests "CLI.Tools" "Azure DevOps CLI"
--- images/win/scripts/Installers/Install-AzureDevOpsCli.ps1
+++ images/win/scripts/Installers/Install-AzureDevOpsCli.ps1
@@ -4,5 +4,14 @@
 ################################################################################
 
 az extension add -n azure-devops
-
+az aks install-cli
+$targetDir = "$env:USERPROFILE\.azure-kubelogin"
+$oldPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
+$oldPathArray = ($oldPath) -split ";"
+if (-Not($oldPathArray -Contains "$targetDir")) {
+    Write-Host "Permanently adding $targetDir to User Path"
+    $newPath = "$oldPath;$targetDir" -replace ";+", ";"
+    [System.Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "User"), [System.Environment]::GetEnvironmentVariable("Path", "Machine") -join ";"
+}
 Invoke-PesterTests -TestFile "CLI.Tools" -TestName "Azure DevOps CLI"
