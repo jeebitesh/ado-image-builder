5522b75666366a50aa4996206b0014175bf05efe
Kalantri <624604@cognizant.com>
2023-12-12 19:09:55 +0530

    adding ansible to linux image

---
+++ images/linux/scripts/installers/ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+################################################################################
+##  File:  ansible.sh
+##  Desc:  Installs ansible
+################################################################################
+
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansiblegit reset
