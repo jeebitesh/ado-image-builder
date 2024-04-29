882b1817da4d7fd36d39a29fe69549b254fbef44
Kalantri <624604@cognizant.com>
2024-04-29 17:37:47 +0530

    refine, refactor scripts

--- images/win/scripts/Installers/Install-Choco.ps1
+++ images/win/scripts/Installers/Install-Choco.ps1
@@ -1,4 +1,5 @@
 Write-Host "Set TLS1.2"
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
 Write-Host "Install chocolatey"
